49ee4fb4f1467d5ecca4c2bda57a93c25b44fedd
hmyfly <hmyself1986@gmail.com>
2022-02-16 16:51:38 -0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,42 @@
+import time
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.tracer(0)
+
+
+player = Player()
+car_manager = CarManager()
+scoreboard = Scoreboard()
+
+
+screen.listen()
+screen.onkey(player.go_up, "Up")
+
+game_is_on = True
+
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+
+    car_manager.create_car()
+    car_manager.move_cars()
+
+    # detect player collide with a car
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+            scoreboard.game_over()
+
+    # detect when the player reaches top
+    if player.ycor() > 280:
+        player.goto(0, -280)
+        car_manager.level_up()
+        scoreboard.new_level()
+
+
+screen.exitonclick()
